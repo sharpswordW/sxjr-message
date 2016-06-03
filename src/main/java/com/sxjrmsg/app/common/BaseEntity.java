c2e769fe86823886ff1548ef5cc2786e671ccdc8
wangrq <596685594@qq.com>
2016-06-03 15:04:57 +0800

    add resource

---
+++ src/main/java/com/sxjrmsg/app/common/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.sxjrmsg.app.common;
+
+import java.sql.Date;
+
+public class BaseEntity {
+	
+	private Date createDate;
+	
+	private String createBy;
+	
+	private Date updateDate;
+	
+	private String updateBy;
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+}
---
+++ src/main/java/com/sxjrmsg/app/msgmodel/entity/MsgModel.java
@@ -0,0 +1,17 @@
+package com.sxjrmsg.app.msgmodel.entity;
+
+import com.sxjrmsg.app.common.BaseEntity;
+
+public class MsgModel extends BaseEntity{
+	
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
