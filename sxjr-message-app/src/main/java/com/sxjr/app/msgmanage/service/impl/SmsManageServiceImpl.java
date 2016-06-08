093930f980b7aab4396166ef25c82b443521218e
outh <outh@sxgroup.com>
2016-06-08 09:49:44 +0800

    update

--- sxjr-message-app/src/main/java/com/sxjr/app/msgmanage/service/impl/SmsManageServiceImpl.java
+++ sxjr-message-app/src/main/java/com/sxjr/app/msgmanage/service/impl/SmsManageServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.sxjr.app.msgmanage.entity.SmClientInfo;
 import com.sxjr.app.msgmanage.mapper.TbSmsManageMapper;
@@ -20,16 +21,29 @@ public class SmsManageServiceImpl implements SmsManageService {
 	private RedisUtil redisUtil;
 	private long tiemOut = 60 * 60 * 6;// 单位：s
 
-	private static final String EMPLOYEE_KEY = "smClient.info.";
+	private static final String CLIENT_KEY = "smClient.info.";
 
 	@Override
 	public int addClient(SmClientInfo smClientInfo) {
 		
-		redisUtil.set(EMPLOYEE_KEY + smClientInfo.getAssessToken(),
+		redisUtil.set(CLIENT_KEY + smClientInfo.getAssessToken(),
 				JSONObject.toJSONString(smClientInfo), tiemOut);
+		boolean flag=isExists(smClientInfo.getAssessToken(),smClientInfo.getAssessSecret());
+		System.out.println(flag);
 		return tbSmsManageMapper.save(smClientInfo);
 	}
-
+	public boolean isExists(String key,String screteID) {
+		 Object result = redisUtil.get(CLIENT_KEY+key);
+		 if(result != null ){
+	            SmClientInfo smClientInfo= JSON.parseObject(result.toString(),SmClientInfo.class);
+	            if (screteID.equals(smClientInfo.getAssessSecret()) && "0".equals(smClientInfo.getStatus())) {
+					return true;
+				}
+	        }
+		
+		return false;
+		
+	}
 	@Override
 	public List<SmClientInfo> list() {
 
--- sxjr-message-interface/src/main/java/org/sxjr/message/inerface/service/QrySmsClient.java
+++ sxjr-message-interface/src/main/java/org/sxjr/message/inerface/service/QrySmsClient.java
@@ -9,14 +9,14 @@ import com.sxjr.common.util.RedisUtil;
 public class QrySmsClient {
 
 	@Autowired
-	private RedisUtil redisUtil;
-	
+	private static RedisUtil redisUtil;
+	private static final String STATUS="1";
 	private static final String CLIENT_KEY = "smClient.info.";
-	public boolean isExists(String key,String screteID) {
+	public static boolean isExists(String key,String screteID) {
 		 Object result = redisUtil.get(CLIENT_KEY+key);
 		 if(result != null ){
 	            SmClientInfo smClientInfo= JSON.parseObject(result.toString(),SmClientInfo.class);
-	            if (screteID.equals(smClientInfo.getAssessSecret()) && "0".equals(smClientInfo.getStatus())) {
+	            if (screteID.equals(smClientInfo.getAssessSecret()) && STATUS.equals(smClientInfo.getStatus())) {
 					return true;
 				}
 	        }
@@ -24,10 +24,5 @@ public class QrySmsClient {
 		return false;
 		
 	}
-	public RedisUtil getRedisUtil() {
-		return redisUtil;
-	}
-	public void setRedisUtil(RedisUtil redisUtil) {
-		this.redisUtil = redisUtil;
-	}
+	
 }
