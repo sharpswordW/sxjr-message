173bb329498dcdbdc03b407186d26675e15d7c64
wangrq <wangrq@sxgroup.com>
2016-06-12 11:29:32 +0800

    add processor

---
+++ sxjr-message-app/src/main/java/com/sxjr/app/processor/entity/SysConfig.java
@@ -0,0 +1,85 @@
+package com.sxjr.app.processor.entity;
+
+public class SysConfig {
+	
+	private String clientPhoneCount;
+	
+	private String clientPhoneTemplet;
+	
+	private String clientPhoneTimeSpan;
+	
+	private String clientBulk;
+	
+	private String conduitPhoneSecond;
+	
+	private String conduitBulkSecond;
+	
+	private String conduitPhoneCount;
+	
+	private String conduitPhoneRepeat;
+
+	public String getClientPhoneCount() {
+		return clientPhoneCount;
+	}
+
+	public void setClientPhoneCount(String clientPhoneCount) {
+		this.clientPhoneCount = clientPhoneCount;
+	}
+
+	public String getClientPhoneTemplet() {
+		return clientPhoneTemplet;
+	}
+
+	public void setClientPhoneTemplet(String clientPhoneTemplet) {
+		this.clientPhoneTemplet = clientPhoneTemplet;
+	}
+
+	public String getClientPhoneTimeSpan() {
+		return clientPhoneTimeSpan;
+	}
+
+	public void setClientPhoneTimeSpan(String clientPhoneTimeSpan) {
+		this.clientPhoneTimeSpan = clientPhoneTimeSpan;
+	}
+
+	public String getClientBulk() {
+		return clientBulk;
+	}
+
+	public void setClientBulk(String clientBulk) {
+		this.clientBulk = clientBulk;
+	}
+
+	public String getConduitPhoneSecond() {
+		return conduitPhoneSecond;
+	}
+
+	public void setConduitPhoneSecond(String conduitPhoneSecond) {
+		this.conduitPhoneSecond = conduitPhoneSecond;
+	}
+
+	public String getConduitBulkSecond() {
+		return conduitBulkSecond;
+	}
+
+	public void setConduitBulkSecond(String conduitBulkSecond) {
+		this.conduitBulkSecond = conduitBulkSecond;
+	}
+
+	public String getConduitPhoneCount() {
+		return conduitPhoneCount;
+	}
+
+	public void setConduitPhoneCount(String conduitPhoneCount) {
+		this.conduitPhoneCount = conduitPhoneCount;
+	}
+
+	public String getConduitPhoneRepeat() {
+		return conduitPhoneRepeat;
+	}
+
+	public void setConduitPhoneRepeat(String conduitPhoneRepeat) {
+		this.conduitPhoneRepeat = conduitPhoneRepeat;
+	}
+
+}
