642058c5ec90ee94ca27192296a0a009cb35e89f
outh <outh@sxgroup.com>
2016-06-12 20:29:39 +0800

    modify

--- sxjr-message-app/src/main/java/com/sxjr/app/msgmanage/service/impl/QrySmsClientImpl.java
+++ sxjr-message-app/src/main/java/com/sxjr/app/msgmanage/service/impl/QrySmsClientImpl.java
@@ -1,5 +1,6 @@
 package com.sxjr.app.msgmanage.service.impl;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sxjr.message.inerface.service.IQrySmsClient;
@@ -12,7 +13,7 @@ import com.sxjr.common.util.RedisUtil;
 public class QrySmsClientImpl implements IQrySmsClient{
 
 	@Autowired
-	private static RedisUtil redisUtil;
+	private RedisUtil redisUtil;
 	/**
      * 客户端状态，1表示启用，0表示禁用
      */
@@ -21,6 +22,8 @@ public class QrySmsClientImpl implements IQrySmsClient{
      * 存入redis中的key前缀
      */
 	private static final String CLIENT_KEY = "smClient.info.";
+	
+	private Logger logger = Logger.getLogger(QrySmsClientImpl.class);
 	/**
 	 * 判断客户端信息key是否存在并且判断接入码screteID是否正确和客户端STATUS是否是可用状态
 	 *@param key 客户端接入账号 screteID 接入凭证
@@ -34,10 +37,11 @@ public class QrySmsClientImpl implements IQrySmsClient{
 		 if(result != null ){
 	            SmClientInfo smClientInfo= JSON.parseObject(result.toString(),SmClientInfo.class);
 	            if (screteID.equals(smClientInfo.getAssessSecret()) && smClientInfo.isState()) {
+	            	logger.info("the "+key+" is available");
 					return true;
 				}
 	        }
-		
+		logger.info("the "+key+" is unavailable");
 		return false;
 	}
 	
--- sxjr-message-app/src/main/java/com/sxjr/app/msgmanage/service/impl/SmsManageServiceImpl.java
+++ sxjr-message-app/src/main/java/com/sxjr/app/msgmanage/service/impl/SmsManageServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.sxjr.message.inerface.service.IQrySmsClient;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
